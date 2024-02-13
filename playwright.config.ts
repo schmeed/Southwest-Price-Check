71fa92afb26f70bc39b41e947729e7aa0c6175e3
schmeed <schmeed@gmail.com>
2024-02-13 12:06:38 -0500

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
+  reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
